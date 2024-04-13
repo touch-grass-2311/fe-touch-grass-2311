4eb26aa8e52520a7d59e00868ce526b38a776aaf
Sam Puttman <daputter2@gmail.com>
2024-04-13 18:02:26 -0400

    FIX - Refactored plants_helper methods to better line up with incomig data. Fixed issue in plant_show

--- app/helpers/plants_helper.rb
+++ app/helpers/plants_helper.rb
@@ -1,26 +1,35 @@
 module PlantsHelper
   def display_or_not_available(values)
-    return 'Not available' if values.nil? || values.empty?
-    values.join(', ')
+    return 'No data available' if values.nil? || values.empty? || values == 'N/A' || 'null'
+
+    if values.is_a?(Array)
+      formatted_months = values.map do |month|
+        Date::ABBR_MONTHNAMES.index(month.capitalize).to_s.rjust(2, '0')
+      end
+
+      formatted_months.sort.map { |month| Date::MONTHNAMES[month.to_i] }.join(', ')
+    else
+      values
+    end
   end
 
   def display_ph_range(min, max)
-    if min.present? && max.present?
-      "#{min} - #{max}"
-    elsif min.present?
-      "#{min} - Not available"
-    elsif max.present?
-      "Not available - #{max}"
+    if min.present? && max.present? && min != 'N/A' && max != 'N/A'
+      '#{min} - #{max}'
+    elsif min.present? && min != 'N/A'
+      '#{min} - Not available'
+    elsif max.present? && max != 'N/A'
+      'Not available - #{max}'
     else
-      "Not available"
+      'No data available'
     end
   end
 
   def display_precipitation(value)
-    if value.nil?
-      'Not available'
+    if value.present? && value != 'N/A'
+      '#{value} mm'
     else
-      "#{value} mm"
+      'No data available'
     end
   end
 end
