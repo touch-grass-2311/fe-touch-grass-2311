5bc6fc2dd940f578ce9cfa833ee5d6dea26d104c
Sam Puttman <daputter2@gmail.com>
2024-04-13 18:01:55 -0400

    FIX - issue with data in plant poro resolved

--- app/poros/plant.rb
+++ app/poros/plant.rb
@@ -1,18 +1,19 @@
 class Plant
   attr_reader :id, :common_name, :scientific_name, :family_common_name, :image_url, :edible,
-              :bloom_months, :ph_max, :ph_min, :light, :min_precipitation
+              :bloom_months, :ph_max, :ph_min, :light, :min_precipitation, :family
 
   def initialize(attributes)
     @id = attributes[:id]
     @common_name = attributes[:common_name]
     @scientific_name = attributes[:scientific_name]
+    @family = attributes[:family]
     @family_common_name = attributes[:family_common_name]
     @image_url = attributes[:image_url]
     @edible = attributes[:edible]
-    @bloom_months = attributes.dig(:main_species, :growth, :bloom_months) || []
-    @ph_max = attributes.dig(:main_species, :growth, :ph_max)
-    @ph_min = attributes.dig(:main_species, :growth, :ph_min)
-    @light = attributes.dig(:main_species, :growth, :light)
-    @min_precipitation = attributes.dig(:main_species, :growth, :min_precipitation)
+    @bloom_months = attributes[:bloom_months].presence || []
+    @ph_max = attributes.dig(:ph_max)
+    @ph_min = attributes.dig(:ph_min)
+    @light = attributes.dig(:light)
+    @min_precipitation = attributes.dig(:min_precipitation, :mm)
   end
 end
