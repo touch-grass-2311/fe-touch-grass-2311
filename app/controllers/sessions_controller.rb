d0b67042d24e9be9b75052b6d6c02a38a292daa0
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-14 14:45:09 -0600

    New sessions controller => handles essentially all login functionality; adds user to DB if first time using app; creates a new session for already-registered users; using Github OAuth to login or register/login; re-directs to user dashboard (virtual garden)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,37 @@
 class SessionsController < ApplicationController
-	def new
-		render :new
-	end
-
 	def create
-		user_info = request.env["omniauth.auth"]
+		client_id = ENV["GITHUB_CLIENT_ID"]
+ 		client_secret = ENV["GITHUB_CLIENT_SECRET"]
+		code = params[:code]
+		# move to services/facade
+		conn = Faraday.new(url: "https://github.com", headers: {"Accept": "application/json"})
+		response = conn.post("/login/oauth/access_token") do |req|
+			req.params = {
+				"code": code,
+				"client_id": client_id,
+				"client_secret": client_secret
+			}
+		end
 
-		require 'pry'; binding.pry
-		# session[:user_id] = user_info.id
+		parsed = JSON.parse(response.body, symbolize_names: true) 
+		
+		access_token = parsed[:access_token]
+
+		conn = Faraday.new(
+			url: "https://api.github.com",
+			headers: {
+				"Authorization": "token #{access_token}"
+			}
+		)
+		response = conn.get("/user")
+		
+		user = JSON.parse(response.body, symbolize_names: true)
+		
+		if user[:data] != nil
+			session[:user_id] = user[:id]
+			redirect_to dashboard_path
+		else
+			# Error: Could not find Github account
+		end
 	end
 end
