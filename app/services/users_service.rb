5907f50a6ad738c6cc4c673a6b9845a705761349
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-14 15:22:09 -0600

    New users service with #find_or_create_new method that initiates a call to BE to search for user in DB or create a new one. Returns user data in JSON format

--- app/services/users_service.rb
+++ app/services/users_service.rb
@@ -1,32 +1,16 @@
 class UsersService
-	def self.get_all_users
-		response = conn.get("/api/v1/users")
+	def self.find_or_create_new_user(user_data)
+		response = conn.post("/api/v1/users")
 		JSON.parse(response.body, symbolize_names: true)
 	end
 
-	def self.get_one_user(uid)
-		response = conn.get("/api/v1/users/#{uid}")
-		JSON.parse(response.body, symbolize_names: true)
-	end
-	
-	def self.register_new_user(user_data)
-		conn = Faraday.new(url: "http://localhost:3000") do |f|
+	def self.post_conn 
+		Faraday.new(url: "http://localhost:3000") do |f|
 			f.headers["Content-Type"] = "application/json"
 			f.headers["Accept"] = "*/*"
 			f.params[:uid] = user_data[:uid]
 			f.params[:email] = user_data[:email]
 			f.params[:name] = user_data[:name]
-		end
-		
-		response = conn.post("/api/v1/users")
-
-		JSON.parse(response.body, symbolize_names: true)
-	end
-
-	def self.conn
-		Faraday.new(url: "http://localhost:3000") do |f|
-			f.headers["Content-Type"] = "application/json"
-			f.headers["Accept"] = "application/json"
 		end
 	end
 end
