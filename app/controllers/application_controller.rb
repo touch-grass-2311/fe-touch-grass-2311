ce395afee891584ffb7c25db5acdad6c8bdf2e91
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-14 14:40:34 -0600

    Add current_user helper method to give view and multiple controllers access to the current user's session; Add ctrf protection;

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception
+	helper_method :current_user
+
+	def current_user
+		@current_user = UsersFacade.find_or_create_user(session[:user_id]) if session[:user_id]
+	end
 end
