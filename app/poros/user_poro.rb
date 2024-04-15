69e3a37802d283225cf9599b7baa2230c5337704
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-15 11:46:35 -0600

    new #user_poro to give devs a user ruby object and more options and control over underlying user data

--- app/poros/user_poro.rb
+++ app/poros/user_poro.rb
@@ -1,10 +1,18 @@
 class UserPoro
-	attr_accessor :uid, :name, :email, :access_token
+	attr_accessor :id, :uid, :name, :email, :access_token, :plants
 
-	def initialize(attributes)
-		@uid = attributes[:uid]
-		@name = attributes[:name]
-		@email = attributes[:email]
-		@access_token = attributes[:access_token]
+	def initialize(user_data)
+		@id = user_data[:id]
+		@uid = user_data[:attributes][:uid]
+		@name = user_data[:attributes][:name]
+		@email = user_data[:attributes][:email]
+		@access_token = user_data[:attributes][:access_token]
+		@plants = parse_plants(user_data[:attributes][:plants])
+	end
+
+	def parse_plants(plant_data)
+		plant_data.map do |plant|
+			eval(plant)
+		end
 	end
 end
