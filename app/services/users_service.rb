444fce4df0d15ef220c66c8337273f19cb1759aa
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-15 11:52:57 -0600

    Refactor; remove all unnecessary conn.get requests; new #find_or_create_user method that's very similar to AR's #find_or_create_by method , users stored on BE, so custom one created that achieves same result; adds new user to DB if they don't already exist, returns current user data (along with most recent state of their virtual garden)

--- app/services/users_service.rb
+++ app/services/users_service.rb
@@ -1,16 +1,18 @@
 class UsersService
-	def self.find_or_create_new_user(user_data)
-		response = conn.post("/api/v1/users")
+	def self.find_or_create_user(user_data)
+		response = conn.post("/api/v1/users") do |f|
+			f.params[:uid] = user_data[:uid]
+			f.params[:name] = user_data[:name]
+			f.params[:email] = user_data[:email]
+			f.params[:access_token] = user_data[:access_token]
+		end
 		JSON.parse(response.body, symbolize_names: true)
 	end
 
-	def self.post_conn 
+	def self.conn
 		Faraday.new(url: "http://localhost:3000") do |f|
 			f.headers["Content-Type"] = "application/json"
 			f.headers["Accept"] = "*/*"
-			f.params[:uid] = user_data[:uid]
-			f.params[:email] = user_data[:email]
-			f.params[:name] = user_data[:name]
 		end
 	end
 end
