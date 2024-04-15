8b7e106c9f9b8522d97d83be84e1a20d579403a3
Sam Puttman <daputter2@gmail.com>
2024-04-15 14:15:23 -0400

    Refactor plant poro and controller - data flow is fixed!

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -2,13 +2,14 @@ class PlantsController < ApplicationController
 
   def index
     response = PlantService.get_all_plants
-    @plants = response[:data].map { |plant_data| Plant.new(plant_data[:attributes]) }
+    @plants = response[:data].map { |plant| Plant.new(plant) }
   end
 
   def show
     response = PlantService.get_plant(params[:id])
     if response.present? && response[:data].present?
-      @plant = Plant.new(response[:data][:attributes])
+      @plant = Plant.new(response[:data])
+
     else
       render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
     end
@@ -16,8 +17,7 @@ class PlantsController < ApplicationController
 
   def search
     response = PlantService.search_plants(params[:q])
-    @plants = response[:data].map { |plant_data| Plant.new(plant_data[:attributes]) }
+    @plants = response[:data].map { |plant| Plant.new(plant[:attributes]) }
     render :index
   end
-
 end
