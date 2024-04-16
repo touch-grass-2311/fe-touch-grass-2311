ce6582ca26aa4277fd699fbc97742f93157abf81
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-16 01:42:38 -0600

    Changed name of users service to sessions service to better reflect the resource for which it is doing most of the work

--- spec/services/sessions_service_spec.rb
+++ spec/services/sessions_service_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe "Users Service", type: :service do
+RSpec.describe "Sessions Service", type: :service do
 	it "can get the info of anyone who registered for the app" do
 		user_uid = "13741144"
 		res = SessionsService.find_or_register_user[:user_uid]
