8e8f2eade808eba4f6c0547298809c8f20e85f04
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-17 12:31:04 -0600

    Remove commented lines

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,6 @@ class SessionsController < ApplicationController
 		client_id = ENV["GITHUB_CLIENT_ID"]
  		client_secret = ENV["GITHUB_CLIENT_SECRET"]
 		code = params[:code]
-		# move to services/facade
 		conn = Faraday.new(url: "https://github.com", headers: {"Accept": "application/json"})
 		response = conn.post("/login/oauth/access_token") do |req|
 			req.params = {
@@ -32,8 +31,6 @@ class SessionsController < ApplicationController
 		session[:avatar_url] = user[:avatar_url]
 		session[:access_token] = access_token
 		
-		# TODO UsersFacade.find_or_create_user()
 		redirect_to dashboard_path(user[:id])
-		# TODO Error: Could not find Github account
 	end
 end
