be793bdc47a0302bbd10d60eda58aae722a62c43
Matt Darlington <matthewsdarlington@gmail.com>
2024-04-17 22:02:36 -0600

    Convert user_data to cookies to obfuscate data to avoid any potential security breaches

--- app/poros/user_poro.rb
+++ app/poros/user_poro.rb
@@ -2,6 +2,21 @@ class UserPoro
 	attr_accessor :id, :uid, :name, :email, :avatar_url, :access_token, :plants
 
 	def initialize(user_data)
+		cookies.encrypted[:user_id] = user_data[:id]
+		user_id = cookies.encrypted[:user_id]
+		cookies.encrypted[:uid] = user_data[:attributes][:uid]
+		uid = cookies.encrypted[:uid]
+		cookies.encrypted[:name] = user_data[:attributes][:name]
+		name = cookies.encrypted[:name]
+		cookies.encrypted[:email] = user_data[:attributes][:email]
+		email = cookies.encrypted[:email] 
+		cookies.encrypted[:avatar_url] = user_data[:attributes][:avatar_url]
+		avatar_url = cookies.encrypted[:avatar_url]
+		cookies.encrypted[:access_token] = user_data[:attributes][:access_token]
+		access_token = cookies.encrypted[:access_token]
+		cookies.encrypted[:plants] = user_data[:attributes][:plant]
+		plants = cookies.encrypted[:plants]
+
 		@id = user_data[:id]
 		@uid = user_data[:attributes][:uid]
 		@name = user_data[:attributes][:name]
